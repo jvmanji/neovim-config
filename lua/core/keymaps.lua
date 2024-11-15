8ba5b28f4a269b0b11b9769594530bb8b7eebaad
Rafał Toboła <rafal.tobola@modivo.com>
2024-11-15 16:48:59 +0100

    magic vim run

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -21,7 +21,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-
+vim.keymap.set('v', '<c-r>', ':\'<,\'>!~/.config/nvim/bin/vim-exec<CR>')
 
 
 -- -- Use an on_attach function to only map the following keys
